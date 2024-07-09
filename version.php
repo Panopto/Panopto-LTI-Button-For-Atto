e188804fb15b5cae98e5090f2ef854dd8c32f117
Adis <adis.hrnjica@gmail.com>
2024-07-09 23:02:32 +0200

    2024070900 release code

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // The current plugin version (Date: YYYYMMDDXX).
-$plugin->version = 2024012500;
+$plugin->version = 2024070900;
 
 // Requires this Moodle version - 3.9.0.
 $plugin->requires = 2020061500;
